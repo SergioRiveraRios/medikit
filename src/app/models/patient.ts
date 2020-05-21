c4e0483ed275d60fe5d3aaee170d2b2c037be550
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-05-20 20:07:24 -0600

    modelo agregado

---
+++ src/app/models/patient.ts
@@ -0,0 +1,13 @@
+export class Patient {
+    name: string;
+    age: number;
+    sex: string;
+    civil: string;
+    origin: string;
+    dom: string;
+    telephone: string;
+    idNumber: number;
+    family: string;
+    status: boolean;
+    next: string;
+}
