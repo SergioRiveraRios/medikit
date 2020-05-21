2ca098fdd5de1c8431d185fd8a227a0972f698de
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-05-20 19:10:39 -0600

    se agrega tabs

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'list',
+        loadChildren: () => import('../pages/list/list.module').then(m => m.ListPageModule)
+      },
+      {
+        path: 'new-product',
+        loadChildren: () => import('../pages/new-product/new-product.module').then(m => m.NewProductPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/list',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/list',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
