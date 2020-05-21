a7da806304476a56e22754fb3615ab00a53f8457
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-05-20 20:09:07 -0600

    interfaz mejorada

--- src/app/pages/view-patients/view-patients.page.ts
+++ src/app/pages/view-patients/view-patients.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Patient} from 'src/app/models/patient'
 
 @Component({
   selector: 'app-view-patients',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-patients.page.scss'],
 })
 export class ViewPatientsPage implements OnInit {
-
   constructor() { }
-
+  patients: any=[
+    {
+      name:'Sergio',
+      age:19,
+      sex:'Masculino',
+      civil:'Soltero',
+      origin:'Tepic,Nayarit',
+      dom:'Helechos #130',
+      telephone:'3112022118',
+      id_number:12332123,
+      family:'',
+      next:'20/05/2020',
+      status:true
+    },
+    {
+      name:'Sergio',
+      age:19,
+      sex:'Masculino',
+      civil:'Soltero',
+      origin:'Tepic,Nayarit',
+      dom:'Helechos #130',
+      telephone:'3112022118',
+      id_number:12332123,
+      family:'',
+      next:'20/05/2020',
+      status:true
+    }
+  ]
   ngOnInit() {
   }
-
+  
 }
