b03a7a92e6f373e62b128d3998fd28b118f9a2d2
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-05-25 22:12:36 -0600

    modificado codigo

--- src/app/pages/history-patients/history-patients.page.ts
+++ src/app/pages/history-patients/history-patients.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from 'src/app/models/appointment';
 
 @Component({
   selector: 'app-history-patients',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-patients.page.scss'],
 })
 export class HistoryPatientsPage implements OnInit {
-
+  appointments: Appointment[] = new Array();
   constructor() { }
 
   ngOnInit() {
+    this.test();
   }
-
+  test(){
+    this.appointments.push({
+      idMedic:'1232323',
+      idPatient:'43434212',
+      fechaCon:'30/03/2020',
+      descrip:'Necesita revisión dermátologica'
+    },{
+      idMedic:'03212332',
+      idPatient:'5123421',
+      fechaCon:'03/04/2020',
+      descrip:'Traer jeringas'
+    })
+  }
 }
