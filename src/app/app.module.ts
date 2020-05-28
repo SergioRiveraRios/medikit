9bfa2f85d29a7505c5a4ee7f2299c9a126d4bbcc
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-05-27 22:30:37 -0600

    firebase config agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule, AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFirestore,
+    AngularFireAuthModule,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
