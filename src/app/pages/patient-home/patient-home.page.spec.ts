4aec3b02254eac2be6f9ae8f7ba91388aba09f7c
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-01 19:49:36 -0600

    interfaz mejorada

---
+++ src/app/pages/patient-home/patient-home.page.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { PatientHomePage } from './patient-home.page';
+
+describe('PatientHomePage', () => {
+  let component: PatientHomePage;
+  let fixture: ComponentFixture<PatientHomePage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PatientHomePage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PatientHomePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
