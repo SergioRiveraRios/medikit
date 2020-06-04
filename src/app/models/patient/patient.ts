179d30bd22fbcb33458263c38221c395d9bf68e3
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-04 12:50:23 -0600

    Modelo modificado

--- src/app/models/patient/patient.ts
+++ src/app/models/patient/patient.ts
@@ -1,16 +1,18 @@
 export class Patient {
-    name: string;       // nombre
-    age: number;        // edad
-    sex: string;        // Hombre,Mujer, Otro:espesificar
-    height: string;     // altura
-    weight: string;     // peso
-    civil: string;      // estado civil
-    origin: string;     // Lugar de origen
-    dom: string;        // Domicilio actual
-    telephone: string;  // telefono actual
-    next: string;       // Siguiente cita
-    blood: string;      // tipo de sangre
-    alergic: [];    // alergias
-    email: string;
-    password: string;
+    id?: string;
+    name?: string;       // nombre
+    age?: number;        // edad
+    sex?: string;        // Hombre,Mujer, Otro:espesificar
+    height?: string;     // altura
+    weight?: string;     // peso
+    civil?: string;      // estado civil
+    origin?: string;     // Lugar de origen
+    dom?: string;        // Domicilio actual
+    telephone?: string;  // telefono actual
+    next?: string;       // Siguiente cita
+    blood?: string;      // tipo de sangre
+    alergic?: [];    // alergias
+    email?: string;
+    password?: string;
+    medic?: string;   //Medico que lo atiende
 }
