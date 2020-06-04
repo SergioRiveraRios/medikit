d865a563e5068ec41376baf03b06509b2ebc57a3
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-04 12:55:54 -0600

    Funcionamiento modificado

--- src/app/services/newPatient/new-patient.service.ts
+++ src/app/services/newPatient/new-patient.service.ts
@@ -1,31 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ReflectiveKey } from '@angular/core';
 
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 import { Patient } from 'src/app/models/patient/patient'
-import {Doctor} from 'src/app/doctorModel/doctor'
+import { Doctor } from 'src/app/doctorModel/doctor'
 @Injectable({
   providedIn: 'root'
 })
 export class NewPatientService {
   public isLogged: any = false;
-  currentDoctor:Doctor;
+  public currentDoctor: string;
+  public uid: ReflectiveKey;
   constructor(private firestore: AngularFirestore, public auser: AngularFireAuth) {
+
   }
-  
-  newPatient(patient: Patient) {
+  newPatient(patient: Patient, doctor: string) {
     this.auser.createUserWithEmailAndPassword(patient.email, patient.password).then(cred => {
-      return this.firestore.collection('patient').doc(cred.user.uid).set({
-        dom: 'asd',
-        idCard: '321232232',
-        name: 'sergio',
-        pass: '123123',
-        patients: ['asd'],
-        schedule: '5:00',
-        telephone: '311231232'
+      return this.firestore.collection('patient').doc(cred.user.uid).set({ 
+      name:patient.name,        // nombre
+      age:patient.age,         // edad
+      sex:patient.sex,        // Hombre,Mujer, Otro:espesificar
+      height:patient.height,      // altura
+      weight:patient.weight,      // peso
+      civil:patient.civil,       // estado civil
+      origin:patient.origin,    // Lugar de origen
+      dom:patient.dom,         // Domicilio actual
+      telephone:patient.telephone,   // telefono actual
+      blood:patient.blood,  // alergias
+      email:patient.email, 
+      password:patient.password, 
+      medic:doctor
       })
     })
-
   }
 }
