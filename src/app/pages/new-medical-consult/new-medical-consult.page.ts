bbb37bf1141ed8277b00e60ac14b3b04066bfc2b
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-11 14:13:16 -0600

    Funcionamiento modificado

--- src/app/pages/new-medical-consult/new-medical-consult.page.ts
+++ src/app/pages/new-medical-consult/new-medical-consult.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, ActivatedRoute } from '@angular/router';
 
+import {MedicalConsultation } from 'src/app/models/medicalConsultation/medical-consultation'
 @Component({
   selector: 'app-new-medical-consult',
   templateUrl: './new-medical-consult.page.html',
   styleUrls: ['./new-medical-consult.page.scss'],
 })
 export class NewMedicalConsultPage implements OnInit {
-
-  constructor() { }
+  public medicalConsult:MedicalConsultation;
+  constructor(private actrouter:ActivatedRoute) { 
+    
+    this.getAppointment();
+  }
 
   ngOnInit() {
   }
-
+  getAppointment(){
+    this.actrouter.queryParams.subscribe(
+      params => {
+        this.medicalConsult = JSON.parse(params.special) as MedicalConsultation;
+      }// params
+    ); // actrouter
+  }
 }
