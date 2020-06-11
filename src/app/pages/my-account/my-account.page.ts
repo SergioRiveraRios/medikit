a024772a88fc0026c3b87b9c20eb0a9398cd646a
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-11 02:32:05 -0600

    Funcionamiento modificado

--- src/app/pages/my-account/my-account.page.ts
+++ src/app/pages/my-account/my-account.page.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Doctor} from 'src/app/doctorModel/doctor'
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore'
+import { MyAccountService } from 'src/app/services/myAccount/my-account.service'
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.page.html',
   styleUrls: ['./my-account.page.scss'],
 })
 export class MyAccountPage implements OnInit {
-
-  constructor() { }
+  public doctor: Doctor;
+  public isLogged: any = false;
+  public userId: string;
+  constructor(private firestore: AngularFirestore,
+              private auser: AngularFireAuth,
+              private accountService: MyAccountService) {
+                this.getCurrentUser();
+               }
 
   ngOnInit() {
   }
-
+  getCurrentUser(){
+    this.doctor = JSON.parse(localStorage.getItem('myData')) as Doctor;
+    console.log("Did data load in appointment : ", this.doctor);
+  }
 }
