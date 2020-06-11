d1acab017fb9ca54a1d87d80d1f92ae49bfc5cec
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-11 02:28:44 -0600

    Tabs modificadas

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { Doctor} from 'src/app/doctorModel/doctor'
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
-
-  constructor() {}
-
+  doctor:Doctor;
+  constructor(private router:Router,private actrouter:ActivatedRoute) {
+    this.actrouter.queryParams.subscribe(
+                params => {
+                  if(params && params.special){
+                    this.doctor=JSON.parse(params.special) as Doctor;
+                    this.actrouter.snapshot.data[params.special]
+                    console.log( this.actrouter.snapshot.data[params.special])
+                  }
+                }
+              )
+  }
 }
