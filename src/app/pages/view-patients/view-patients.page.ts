af9fed4ba9e4c5bc645001b45bdf2e1364346c59
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-11 02:31:06 -0600

    Funciones modificadas

--- src/app/pages/view-patients/view-patients.page.ts
+++ src/app/pages/view-patients/view-patients.page.ts
@@ -1,42 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from 'src/app/models/patient/patient';
-import { NavigationExtras, Router } from '@angular/router';
+import { NavigationExtras, Router, ActivatedRoute } from '@angular/router';
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore'
 
 import { Doctor } from 'src/app/doctorModel/doctor'
-import {ViewpatientsService} from 'src/app/services/viewpatients/viewpatients.service'
+import { ViewpatientsService } from 'src/app/services/viewpatients/viewpatients.service'
 @Component({
   selector: 'app-view-patients',
   templateUrl: './view-patients.page.html',
   styleUrls: ['./view-patients.page.scss'],
 })
 export class ViewPatientsPage implements OnInit {
-  public patients: Patient[] ;
+  public patients: Patient[];
   public isLogged: any = false;
-  public  userId:string;
+  doctor: Doctor;
+
   constructor(private router: Router,
               public auser: AngularFireAuth,
-              private db: AngularFirestore,
-              private viewService: ViewpatientsService) {
+              private viewService: ViewpatientsService,
+              private actrouter: ActivatedRoute) {
+              this.doctor = JSON.parse(localStorage.getItem('myData')) as Doctor;
+              console.log("Did data load? : ",this.doctor);
+              this.getpatientsCollection(this.doctor.id)
+              
   }
   ngOnInit() {
-    if(this.isLogged){
-      console.log("usuario ya logeado")
-      console.log(this.userId);
-      this.getpatientsCollection(this.userId);
-    }else{
-      this.auser.authState.subscribe( user => {
-        if (user) { 
-          this.userId = user.uid
-          console.log(this.userId);
-          this.isLogged=true;
-          this.getpatientsCollection(this.userId);
-        }
-      });
-    }
-    
+
   }
   viewPatient(patient: Patient): void {
     const extras: NavigationExtras = {
@@ -46,18 +37,27 @@ export class ViewPatientsPage implements OnInit {
     };
     this.router.navigate(['../detail-patient'], extras);
   }
-
+  viewAppointments(): void {
+    const extras: NavigationExtras = {
+      queryParams: {
+        special: JSON.stringify(this.doctor)
+      }
+    };
+    this.router.navigate(['/tabs'], extras);
+  }
   newPatient() {
     this.router.navigate(['../add-patient']);
   }
-  getpatientsCollection(doctorID:string){
+  getpatientsCollection(doctorID: string) {
     this.viewService.getPatient(doctorID).subscribe(data => {
-        this.patients = data.map(e => {
-          return{
-            id: e.payload.doc.id,
-            ...e.payload.doc.data(),
-          } as Patient;
-        })
-      });
+      this.patients = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Patient;
+      })
+    });
   }
+
+
 }
