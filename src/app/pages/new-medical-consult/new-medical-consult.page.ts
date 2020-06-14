3b1f957bc7ad6c37eca4109577f822911d0d41d2
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-14 01:14:24 -0600

    Funcionamiento modificado

--- src/app/pages/new-medical-consult/new-medical-consult.page.ts
+++ src/app/pages/new-medical-consult/new-medical-consult.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, ActivatedRoute } from '@angular/router';
 
 import {MedicalConsultation } from 'src/app/models/medicalConsultation/medical-consultation'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {NewMedicalConsultService} from 'src/app/services/newMedicalConsult/new-medical-consult.service' 
 @Component({
   selector: 'app-new-medical-consult',
   templateUrl: './new-medical-consult.page.html',
@@ -9,18 +11,51 @@ import {MedicalConsultation } from 'src/app/models/medicalConsultation/medical-c
 })
 export class NewMedicalConsultPage implements OnInit {
   public medicalConsult:MedicalConsultation;
-  constructor(private actrouter:ActivatedRoute) { 
+  public myForm: FormGroup;
+  medicamentos: string[] = new Array();
+  constructor(private actrouter:ActivatedRoute,
+    private form: FormBuilder,
+    private MedicalService:NewMedicalConsultService) { 
     
     this.getAppointment();
   }
 
   ngOnInit() {
+    this.validations();
+  }
+  validations() {
+    this.myForm = this.form.group({
+      descrip: ['', Validators.compose([
+        Validators.required
+      ])],
+      medicaments: ['', Validators.compose([
+        Validators.required
+      ])]
+    });
   }
   getAppointment(){
     this.actrouter.queryParams.subscribe(
       params => {
         this.medicalConsult = JSON.parse(params.special) as MedicalConsultation;
+        console.log(this.medicalConsult)
       }// params
     ); // actrouter
   }
+  addMedicament() {
+    this.medicamentos.push(this.myForm.get('medicaments').value);
+    this.myForm.get('medicaments').reset();
+  }
+
+  /*setMedical(){
+    this.medicalConsult = {
+    description:this.myForm.get('descrip').value, 
+    treatment: this.medicamentos,   
+    date: this.medicalConsult.date,
+    appointmentID: this.medicalConsult.idappointment,
+    medicID: this.medicalConsult.idMedic,
+    patientID: this.medicalConsult.patientID,
+    patientName: this.medicalConsult.patientName
+    }
+    this.MedicalService.newMedical(this.medicalConsult);
+  }*/
 }
