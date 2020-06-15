f26785ef9c30b7d5a087268240b66be3453f6529
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-15 02:57:15 -0600

    Modelo modificado

--- src/app/models/medicalConsultation/medical-consultation.ts
+++ src/app/models/medicalConsultation/medical-consultation.ts
@@ -5,5 +5,6 @@ export class MedicalConsultation {
     descrip?: string;        // Descripciones/valoraciones por consulta
     resipe?: string[];         // recetas
     date?:string;
+    time?:string;
     patientName:string;
 }
