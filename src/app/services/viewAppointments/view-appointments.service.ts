46eacd2010cd2b26214521e781acf73e738afe9e
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-15 02:49:45 -0600

    Servicio modificado

--- src/app/services/viewAppointments/view-appointments.service.ts
+++ src/app/services/viewAppointments/view-appointments.service.ts
@@ -3,6 +3,7 @@ import {AngularFirestore} from '@angular/fire/firestore'
 import {AngularFireDatabase} from '@angular/fire/database'
 
 import { Appointment } from 'src/app/models/appointment/appointment';
+import { Patient } from 'src/app/models/patient/patient';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,4 +17,8 @@ export class ViewAppointmentsService {
      ref=>ref.where('idMedic', '==', doctorID).where('status','==',true)).snapshotChanges()
   }
   
+  getPatientAppointments(patientID:string){
+    return this.firestore.collection('appointment',
+     ref=>ref.where('idPatient', '==', patientID)).snapshotChanges()
+  }
 }
