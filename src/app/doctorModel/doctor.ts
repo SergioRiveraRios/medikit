ee41f22407b4d1e5aca2a6e4198ae28c6269e87d
Sergio Ernesto Rivera Rios <seerriverari@ittepic.edu.mx>
2020-06-17 00:47:11 -0600

    Modelo modificado

--- src/app/doctorModel/doctor.ts
+++ src/app/doctorModel/doctor.ts
@@ -1,9 +1,12 @@
 export class Doctor {
     id?: string;
-    name: string;       // nombre del doctor
-    email: string;      // correo Electronico
-    password: string;       // contraceña
-    dom: string;        // Domicilio
-    telephone: string;  // telefono
-    idCard: string;     // Cedula
+    name?: string;       // nombre del doctor
+    email?: string;      // correo Electronico
+    password?: string;       // contraceña
+    dom?: string;        // Domicilio
+    telephone?: string;  // telefono
+    idCard?: string;     // Cedula
+    link?: string;
+    sex?: string;         // Hombre,Mujer, Otro:espesificar
+    blood?: string;  // alergias
 }
